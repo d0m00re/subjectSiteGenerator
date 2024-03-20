13bd65b07d8451e82107184429e057e7b478a956
Your Name <you@example.com>
2024-03-20 14:55:01 +0100

    (~) rework header style

--- generator/src/components/SignInButton.tsx
+++ generator/src/components/SignInButton.tsx
@@ -2,20 +2,50 @@
 
 import { useSession } from 'next-auth/react'
 import Link from 'next/link';
-import React from 'react'
-
+import React from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu"
+import { Settings } from 'lucide-react';
 type Props = {}
-
+type Checked = DropdownMenuCheckboxItemProps["checked"]
 const SignInButton = ({ }: Props) => {
     const { data: session } = useSession();
+    const [showStatusBar, setShowStatusBar] = React.useState<Checked>(true)
+    const [showActivityBar, setShowActivityBar] = React.useState<Checked>(false)
+    const [showPanel, setShowPanel] = React.useState<Checked>(false)
 
     if (session && session.user)
         return (
-            <div className="flex gap-4 ml-auto">
-                <p className='text-sky-600'>{session.user.name}</p>
-                <Link href="/api/auth/signout" className='flex gap-4 ml-auto text-red-600'>
-                    Sign Out
-                </Link>
+            <div className="flex items-center gap-4 ml-auto">
+                <Badge># Free tier</Badge>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Avatar className=' cursor-pointer'>
+                            <AvatarImage src="https://github.com/shadcn.png" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56">
+                        <DropdownMenuItem>
+                            <Link href="/api/auth/signout" className='flex w-full gap-4 ml-auto text-red-600'>
+                                Sign Out
+                            </Link>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
         )
 
