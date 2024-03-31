19bffb84564dc4fc593e56943386a1b05448481e
Your Name <you@example.com>
2024-03-31 12:00:06 +0200

    (+) add base api request

--- server/src/theme/theme-group/theme-group.controller.ts
+++ server/src/theme/theme-group/theme-group.controller.ts
@@ -1,9 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ThemeGroupService } from './theme-group.service';
+import * as dto from "./dto/themeGroup.dto";
+import { JwtCookieParserGuard } from 'src/authv2/guard/jwt-cookie-parser.guard';
+
 
 @Controller('theme-group')
 export class ThemeGroupController {
     constructor(
         private themeGroupService: ThemeGroupService
       ) { }
+
+        // create
+        @UseGuards(JwtCookieParserGuard)
+        @Post()
+        async create(@Body() dto : dto.CreateThemeGroupDto) {
+            let data = await this.themeGroupService.create({
+                name : dto.name
+            })
+
+            return data;
+        }
+        // get all
+        @UseGuards(JwtCookieParserGuard)
+        @Get()
+        async getAll() {
+            let data = await this.themeGroupService.getAll();
+            return data;
+        }
 }
--- server/src/theme/theme-palette/theme-palette.controller.ts
+++ server/src/theme/theme-palette/theme-palette.controller.ts
@@ -1,9 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ThemePaletteService } from './theme-palette.service';
+import * as dto from "./dto/themePalette.dto";
+import { JwtCookieParserGuard } from 'src/authv2/guard/jwt-cookie-parser.guard';
 
 @Controller('theme-palette')
 export class ThemePaletteController {
     constructor(
         private themePaletteService: ThemePaletteService
       ) { }
+   //   async create(@Body() dto : dto.CreateThemeGroupDto) {
+
+    @UseGuards(JwtCookieParserGuard)
+    @Post("")
+     async createWithPalette(@Body() dto : dto.CreatePaletteWithElem) {
+        let data = await this.themePaletteService.createWtPaletteElem({
+            themeGroupId : dto.themeGroupId,
+            name : dto.name,
+            paletteElements : dto.paletteElements
+        });
+
+        return data;
+    }
+
 }
