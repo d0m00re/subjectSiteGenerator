5ec71bcb3d86214cbc40286ae76b1b724d8713ff
Your Name <you@example.com>
2024-04-02 00:49:17 +0200

    (~) fix website view

--- generator/src/app/view/[id]/page.tsx
+++ generator/src/app/view/[id]/page.tsx
@@ -6,11 +6,9 @@ import useTemplateGroup from '@/store/templateGroup.zustand.store';
 import { useParams } from 'next/navigation';
 import TemplateSkeleton from '@/components/Templates/TemplateSkeleton';
 import RenderSectionWtConfig from '@/components/WebsiteSection/Render/RenderSectionWtConfig';
-import SectionWebsite from '@/app/dashboard/website/[id]/components/SectionWebsite';
 import useCurrentWebsiteStore from '@/app/dashboard/website/[id]/components/store/currentWebsite.zustand.store';
 import MainLayout from '@/components/WebsiteSection/Render/MainLayout/MainLayout';
 import useTemplatePalette from '@/store/templatePalette.zustand.store';
-import { Button } from '@/components/Button';
 
 
 function page() {
@@ -28,22 +26,26 @@ function page() {
         storeTemplate.populate();
         storeTemplatePalette.populate();
         setDataIsLoad(true);
-    }, [])
+    }, []);
 
+    let currentTheme = storeWebsite.website?.themePalette;
+    if (!currentTheme) return <></>
+    let realCurrentTheme = currentTheme;
 
     return (
-            <MainLayout>
-                <>
-                    {(dataIsLoad) ?
-                        storeWebsite?.website?.websiteSection?.map((section, index) =>
-                            <section key={`section-${section.id}`} className={`flex flex-col p-4 gap-2 ${section.backgroundColor}`}>
-                                <RenderSectionWtConfig
-                                    section={section}
-                                />
-                            </section>) : <TemplateSkeleton />
-                    }
-                </>
-            </MainLayout>
+        <MainLayout>
+            <>
+                {(dataIsLoad) ?
+                    storeWebsite?.website?.websiteSection?.map((section, index) =>
+                        <section key={`section-${section.id}`} className={`flex flex-col`}>
+                            <RenderSectionWtConfig
+                                section={section}
+                                themePalette={realCurrentTheme.themePaletteElems[section.themePaletteOrder]}
+                            />
+                        </section>) : <TemplateSkeleton />
+                }
+            </>
+        </MainLayout>
     )
 }
 
